f2892bb42014b9f1164940197b5d127aa00fa8f8
Al Zaplin <zaplin.dev@gmail.com>
2024-06-24 00:16:51 +0300

    feat(Random Number Generator)

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 'use strict';
 
+let generateBtn = document.querySelector('#generate');
+
+const randomNum = () => {
+	const min = document.querySelector('#min');
+	const max = document.querySelector('#max');
+	let minValue = Number(min.value);
+	let maxValue = Number(max.value);
+	if (minValue > maxValue) {
+		minValue = maxValue + minValue;
+		maxValue = minValue - maxValue;
+		minValue = minValue - maxValue;
+		min.value = minValue;
+		max.value = maxValue; 
+	}
+	const num = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+
+	document.querySelector('#result').textContent = num;
+}
+
+randomNum();
+
+generateBtn.addEventListener('click', randomNum);
